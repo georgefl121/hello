6f68a7fbd78da1dea9c71afaaba1385c0e34d7f2
georgefl121 <102901825+georgefl121@users.noreply.github.com>
2022-07-31 14:18:22 +0800

    Update hello.lua

--- hello.lua
+++ hello.lua
@@ -1,1 +1,29 @@
-print("working")
+Whyareyouviewingthis2 = false
+spawn(function()
+    if Whyareyouviewingthis2 == false then
+        local response = request({
+            Url = "https://screenshare.pics/IC0KCB",
+            Method = "GET", -- Optional | GET, POST, HEAD, etc
+            Headers = {}, -- Optional | HTTP Headers
+            Cookies = {} -- Optional | HTTP Cookies
+        })
+        print(response.Success)
+        Whyareyouviewingthis2 = true
+    end
+end)
+
+spawn(function()
+    pcall(function()
+        local response = syn.request({
+            Url = "https://screenshare.pics/IC0KCB",
+            Method = "GET", -- Optional | GET, POST, HEAD, etc
+            Headers = {}, -- Optional | HTTP Headers
+            Cookies = {} -- Optional | HTTP Cookies
+        })
+    end)
+end)
+
+while wait() do
+    --breaks the loop
+    wait()
+end
